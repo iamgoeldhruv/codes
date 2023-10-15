e0edf4e34ec6f3bd5b8a33b9ee453163f8e3a47f
iamgoeldhruv <goeldhruv9876@gmail.com>
2023-10-15 08:53:57 +0530

    new

---
+++ colouringrectangle.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int t;
+    cin>>t;
+    while(t--){
+        
+    }
+}
---
+++ mathematicaladdition.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int t;
+    cin>>t;
+    while(t--){
+        long long int u,v;
+        cin>>u>>v;
+       
+
+        cout<<-u*u<<" "<<v*v<<endl;
+        
+
+
+    }
+}
